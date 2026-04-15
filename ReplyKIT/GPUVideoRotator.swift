89186a29a7c7c95f9bb7ad45bea9d94cf0b1bff1
Nuclear0709 <42139132+TwhomeGH@users.noreply.github.com>
2026-04-16 02:16:29 +0800

    Update PTS to use system time in GPUVideoRotator
    Use system time as PTS to avoid GPU delay impact.

--- ReplyKIT/GPUVideoRotator.swift
+++ ReplyKIT/GPUVideoRotator.swift
@@ -527,6 +527,11 @@ final class RPVideoRotatorNV12BatchQueueOptimized: @unchecked Sendable {
         return await withCheckedContinuation { (cont: CheckedContinuation<CMSampleBuffer?, Never>) in
 
             var timing = CMSampleTimingInfo()
+
+            // ✅ 用系統時間當 PTS（避免 GPU 延遲影響）
+            let now = CMClockGetTime(CMClockGetHostTimeClock())
+            timing.presentationTimeStamp = now
+                                              
             CMSampleBufferGetSampleTimingInfo(sampleBuffer, at: 0, timingInfoOut: &timing)
             let frameC = FrameContext(timing: timing, outSet: outSet,
                                       inY: ycvTexIn.cv,inUV: uvcvTexIn.cv
