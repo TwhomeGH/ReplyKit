188575d82cb73f74ab38f31f4c52fc6fa30d33bb
Nuclear0709 <agp05432@gmail.com>
2026-04-17 13:16:50 +0800

    小調整

--- ReplyKIT/GPUVideoRotator.swift
+++ ReplyKIT/GPUVideoRotator.swift
@@ -570,18 +570,17 @@ final class RPVideoRotatorNV12BatchQueueOptimized: @unchecked Sendable {
                 
                 CVPixelBufferLockBaseAddress(frameC.outPB, [])
                 CVPixelBufferUnlockBaseAddress(frameC.outPB, [])
-                                     
+                
                 frameC.inY = nil
                 frameC.inUV = nil
 
-               
+
                 
-    
-                                                  
+                
                 // ✅ 用系統時間當 PTS（避免 GPU 延遲影響）
                 let now = CMClockGetTime(CMClockGetHostTimeClock())
                 frameC.timing.presentationTimeStamp = now
-               
+
                                             
     
                 audioProcess?.updateVideoPTS(now)
