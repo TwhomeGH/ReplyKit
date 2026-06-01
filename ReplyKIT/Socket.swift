dcd04750f347b7c81877dcc27b2efcd13f14925c
Nuclear0709 <agp05432@gmail.com>
2026-06-01 18:58:57 +0800

    調整語法參數

--- ReplyKIT/Socket.swift
+++ ReplyKIT/Socket.swift
@@ -959,7 +959,7 @@ class SocketClient : @unchecked Sendable {
                     let desc = String(describing: rawValue)
                     logger.debug("UPSet key=\(key) type=\(type(of: rawValue)) value=\(desc) SafeVal:\(safeValue)")
                     
-                    logTo("UPGet -> \(String(describing: safeValue)) \(String(describing: safeValue))")
+                    logTo("UPGet -> \(key) \(String(describing: safeValue))")
 
 
                     Task {
