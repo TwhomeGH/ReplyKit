ea14e205ac8bfe209fe8a115a626a34126242e34
Nuclear0709 <agp05432@gmail.com>
2026-07-17 10:26:02 +0800

    調整audioPage音量可能沒有更新的問題

--- liveAPP/ContentView.swift
+++ liveAPP/ContentView.swift
@@ -210,8 +210,8 @@ struct BroadcastButton: UIViewRepresentable {
 final class LiveVolumeModel: ObservableObject {
     static let shared = LiveVolumeModel()   // 全局共用單例
 
-    var micVolumeLive: Float = 0.0
-    var appVolumeLive: Float = 0.0
+    @Published var micVolumeLive: Float = 0.0
+    @Published var appVolumeLive: Float = 0.0
 
     private init() {
 #if os(iOS)
