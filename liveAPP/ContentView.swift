7cbb287ebad716effb68b8e353e42962af22f07d
Nuclear0709 <agp05432@gmail.com>
2026-07-29 09:38:43 +0800

    修正StreamBtn初始化反覆發生的問題

--- liveAPP/ContentView.swift
+++ liveAPP/ContentView.swift
@@ -2193,11 +2193,12 @@ struct homeView:View{
     @AppStorage("rtmpKey",store: userDefaults) var rtmpKey: String = "stream1?vhost=live2"
     @AppStorage("broadcastExtension",store: userDefaults) var broadcastExtension: String = (Bundle.main.bundleIdentifier ?? "nuclear.liveAPP") + ".ReplyKIT"
 
-    @State private var streamBtn: BroadcastButton = {
-        let ext = (Bundle.main.bundleIdentifier ?? "nuclear.liveAPP") + ".ReplyKIT"
-        sendlog(title: "BroadcastButton", message: "初始化 picker ext=\(ext)")
-        return BroadcastButton(preferredExtension: ext, rtmpURL: "rtmp://192.168.0.102/live", rtmpKey: "stream1?vhost=live2", width: 50, height: 50)
-    }()
+    @State private var streamBtn = BroadcastButton(
+        preferredExtension: (Bundle.main.bundleIdentifier ?? "nuclear.liveAPP") + ".ReplyKIT",
+        rtmpURL: "rtmp://192.168.0.102/live",
+        rtmpKey: "stream1?vhost=live2",
+        width: 50, height: 50
+    )
 
     @StateObject var manager = BitrateManager()
 
